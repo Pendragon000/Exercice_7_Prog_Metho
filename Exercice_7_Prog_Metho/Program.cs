3a1e62c3da6ab72bd1ea07ecc53ba48707df9c09
Isaak Fortin <isaakfortnite56@gmail.com>
2026-04-15 18:04:37 -0400

    Add full code coverage all test pass

--- Exercice_7_Prog_Metho/Program.cs
+++ Exercice_7_Prog_Metho/Program.cs
@@ -2,27 +2,7 @@
 {
     internal class Program
     {
-        static string test = "patate OR has:document AND after:2026/4/10 AND before:2026/4/28 AND test";
         
-        static void Main(string[] args)
-        {
-            List<Courriel> couriels = [];
-            couriels.Add(new Courriel("patate", DateTime.Now, false));
-            couriels.Add(new Courriel("", DateTime.Now, true));
-            couriels.Add(new Courriel("", DateTime.Now, false));
-            couriels.Add(new Courriel("test", DateTime.Today, true));
-            couriels.Add(new Courriel("test", DateTime.Today, false));
-            couriels.Add(new Courriel("test", DateTime.Today, false));
-            Filtre filtre = new(test);
-            List<Courriel> passFiltre = [];
-            foreach (var couriel in couriels)
-            {
-                if (filtre.Filtrer(couriel))
-                {
-                    passFiltre.Add(couriel);
-                }
-            }
-            Console.WriteLine(passFiltre.Count);
-        }
+        public static void Main() {}
     }
 }
--- TestExercice_7_Prog_Metho/Test.cs
+++ TestExercice_7_Prog_Metho/Test.cs
@@ -1,5 +1,6 @@
 ﻿
 using Exercice_7_Prog_Metho;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace TestExercice_7_Prog_Metho
 {
@@ -65,6 +66,33 @@ namespace TestExercice_7_Prog_Metho
             couriels.Add(new Courriel("", DateTime.Today, true));
             couriels.Add(new Courriel("", DateTime.Today, true));
             string test = "before:2025/12/25 OR after:2026/4/13 AND before:2026/4/28 AND test";
+            List<Courriel> passCouriel = [];
+            Filtre filtre = new(test);
+            foreach (var courriel in couriels)
+            {
+                if (filtre.Filtrer(courriel))
+                {
+                    passCouriel.Add(courriel);
+                }
+            }
+            Assert.AreEqual(2, passCouriel.Count);
+            Assert.IsTrue(passCouriel.Contains(cour0));
+            Assert.IsTrue(passCouriel.Contains(cour1));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            List<Courriel> couriels = [];
+            Courriel cour0 = new Courriel("patate", DateTime.Parse("2024/4/20"), false);
+            Courriel cour1 = new Courriel("patate", DateTime.Now, true);
+            couriels.Add(cour0);
+            couriels.Add(cour1);
+            couriels.Add(new Courriel("", DateTime.Now, false));
+            couriels.Add(new Courriel("", DateTime.Today, true));
+            couriels.Add(new Courriel("", DateTime.Today, true));
+            couriels.Add(new Courriel("", DateTime.Today, true));
+            string test = "patate";
             List<Courriel> passCouriel = [];
             Filtre filtre = new(test);
             foreach (var courriel in couriels)
